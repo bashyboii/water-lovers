b67eac962b397c838e9c17215c555ca0def6e3a1
bashyboii <sebastiansmith1185@gmail.com>
2025-09-26 19:41:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template 
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('index.html')
+
+@app.route('/drivemad')
+def drivemad():
+    return render_template('drivemad.html')
+@app.route('/RagdollArchers')
+def ragdollarchers():
+    return render_template('RagdollArchers.html')
+@app.route('/Proxy')
+def proxy():
+    return render_template('Proxy.html')
+@app.route('/EscapeRoad')
+def escaperoad():
+   return render_template('EscapeRoad.html')
+if __name__ == '__main__':
+    app.run() 
